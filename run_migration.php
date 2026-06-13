6163b259a838d41b7c8134cbde38d5c86e2ebad2
Santiago072 <santiago@sena.edu.co>
2026-06-13 17:45:29 -0500

    Cleanup: Remove obsolete migrations folder and script for the old job queue

--- run_migration.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'config/database.php';
-$db = getDB();
-$sql = file_get_contents('migrations/create_logs_importacion_table.sql');
-$db->exec($sql);
-echo "Tabla logs_importacion creada exitosamente en sena_juicios\n";
